4cce26f856a2d50e284efe8096b814e0fb02d422
StephanieB-lgtm <scandicwarrior@gmail.com>
2025-10-21 21:04:04 +0200

    add HU scrapers

---
+++ scrapers/spiders/Hungary/PyTube/budahazyedda/budahazy_edda_for_trancriber_YT.py
@@ -0,0 +1,15 @@
+from .....functions.transcriber_data_functions import Transcriber_data_Functions
+
+''' To run this scraper from bash do the following:
+        cd ./YOU-DARE/scrapers
+
+        python -m scrapers.spiders.Hungary.PyTube.budahazyedda.budahazy_edda_for_trancriber_YT
+'''
+
+transcriber_prep = Transcriber_data_Functions()
+
+transcriber_prep.clean_transcribed_audio_files_setup(
+     base_path='/work/YOU-DARE/scrapers/data/Hungary/budahazy_edda_YT'
+)
+
+
