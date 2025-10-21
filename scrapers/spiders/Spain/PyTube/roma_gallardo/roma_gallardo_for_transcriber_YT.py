070c22e0eb46fb44c8789256a4d1c2cc7de48c67
Josefine Christensen <josefine@familienchristensen.com>
2025-10-21 20:56:34 +0200

    add spanish scrapers

---
+++ scrapers/spiders/Spain/PyTube/roma_gallardo/roma_gallardo_for_transcriber_YT.py
@@ -0,0 +1,12 @@
+from .....functions.transcriber_data_functions import Transcriber_data_Functions
+
+''' To run this scraper from bash do the following:
+        cd ./YOU-DARE/scrapers
+        python -m scrapers.spiders.Spain.PyTube.roma_gallardo.roma_gallardo_for_transcriber_YT
+'''
+
+transcriber_prep = Transcriber_data_Functions()
+
+transcriber_prep.clean_transcribed_audio_files_setup(
+    base_path='/work/YOU-DARE/scrapers/data/Spain/roma_gallardo_YT'
+)
