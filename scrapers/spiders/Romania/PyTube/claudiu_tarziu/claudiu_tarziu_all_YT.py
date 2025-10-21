94762428b4810750c9b7071fae043ff3ab2a0ede
Josefine Christensen <josefine@familienchristensen.com>
2025-10-21 21:19:40 +0200

    add RO scrapers

---
+++ scrapers/spiders/Romania/PyTube/claudiu_tarziu/claudiu_tarziu_all_YT.py
@@ -0,0 +1,35 @@
+from datetime import time
+import sys
+from pathlib import Path
+from .....functions.pytubefix_functions import Pytubefix_Functions
+
+''' To run this scraper from bash do the following:
+        cd ./YOU-DARE/scrapers
+        python -m scrapers.spiders.Romania.PyTube.claudiu_tarziu.claudiu_tarziu_all_YT
+    The first time this script is called after stating up a new UCloud session it will give the following message:
+        Please open https://www.google.com/device and input code RJQ-YVM-HVF
+        Press enter when you have completed this step.
+    After you've entered the website and passed the code you'll be asked which YouTube user you want to proceed as.
+    Without this check and login PyTube can't age verify and will therefore not download any age restricted audio/video.
+'''
+
+# The links of interest
+channel_url = 'https://www.youtube.com/@Claudiu.Tarziu' # videos, shorts and streams
+playlist1 = 'https://www.youtube.com/playlist?list=PLSfRvqOFLFUHnsVwtWTl3k0HEudFqgTkc'
+playlist2 = 'https://www.youtube.com/playlist?list=PLSfRvqOFLFUEBdX9Q0Eor34eYuv7SKdo9'
+playlist3 = 'https://www.youtube.com/playlist?list=PLSfRvqOFLFUH8spjKSSPJNq1CurHrbcdt'
+
+single_video = 'https://www.youtube.com/watch?v=mo8Pc-VsNZQ'
+
+# Generating the proper output path
+generated_output_path = Pytubefix_Functions.generate_output_path(__file__, nesting_level=5)
+
+# Generates a jsonlines file and downloads all audio from all videos and places it on the generated output path
+Pytubefix_Functions.pytubefix_from_channel(channel_url, __file__, nesting_level=5, videos=True, shorts=True, live=True, check_for_downloaded=True)
+Pytubefix_Functions.pytubefix_from_playlist(playlist1, __file__, nesting_level=5, source='Claudiu.Tarziu', check_for_downloaded=True)
+Pytubefix_Functions.pytubefix_from_playlist(playlist2, __file__, nesting_level=5, source='Claudiu.Tarziu', check_for_downloaded=True)
+Pytubefix_Functions.pytubefix_from_playlist(playlist3, __file__, nesting_level=5, source='Claudiu.Tarziu', check_for_downloaded=True)
+Pytubefix_Functions.pytubefix_from_single(single_video, __file__, nesting_level=5, source='Claudiu.Tarziu')
+
+Pytubefix_Functions.retry_failed_downloads(generated_output_path)
+# Pytubefix_Functions.pytubefix_from_channel_jsonlines(channel_url, generated_output_path)
