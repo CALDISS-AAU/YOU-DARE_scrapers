9c140eb46579b051fe342a1a446bc948f26f8924
Matias Appel <matias.appel@outlook.dk>
2025-10-21 21:20:39 +0200

    add UK scrapers

---
+++ scrapers/spiders/United_Kingdom/mallard_dynamic_SPIDER.py
@@ -0,0 +1,161 @@
+### IMPORTS ###
+# External imports #
+import scrapy
+from scrapy import signals
+from scrapy.selector import Selector
+import asyncio
+from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.internet.threads import deferToThread
+from datetime import datetime
+from urllib.parse import urljoin
+# Internal imports #
+from ...items import ScrapersItem  # Imports the items from the items.py file
+from ...functions.scrapy_functions import Dynamic_Scrapy_Click  # Custom shared click functions
+from ...functions.general_functions import General_Functions  # Custom shared functions
+
+''' THIS SPIDER IS NOT ABLE TO RUN!
+    To run this spider pass the following to the terminal:
+        cd ./YOU-DARE/scrapers
+        scrapy crawl template_dynamic_SPIDER -a max_pages=x # MUST MATCH SPIDER NAME!
+    where -a max_pages=x is an optional parameter
+'''
+
+### CREATING THE SPIDER ###
+class DynamicSpider(scrapy.Spider): # Can be changed but it's not necessary - if changed also change Super in from_crawler function
+    name = 'mallard_dynamic_SPIDER' # Spider name - must be unique within given project
+    region = 'United_Kingdom' # Parent folder - used for folderstructure within the data folder - MUST BE IDENTICAL TO SPIDERS DIRECT PARENT FOLDER!
+    source = 'The Mallard' # The source of the articles - NOT the author!
+    start_urls = ['https://mallarduk.com/category/comment/', 'https://mallarduk.com/category/culture/'] # The url where the content to be scraped is found - can be multiple urls IF THE CSS/XPATH IS IDENTICAL!
+
+    ## HTML directions ##
+    ''' These can be both CSS and XPath or a mix as long as it's matched within the response functions within the different parse functions.
+        E.g. 'some_css' must use response.css('some_css') and 'some_xpath' must use response.xpath('some_xpath')
+    '''
+    # QUERIES FROM THE FRONT PAGE!!!
+    ''' CSS or XPath queries for relevant information found on the front page. 
+        For functionality the following queries HAVE to be found on the front page:
+            links_to_follow # The links to the individual articles
+            next_page # The links to the next page
+        How to pass information from the parse_front function to the parse_article function will be described in greater detail later on.
+    '''
+    links_to_follow_CSS = "h2 a::attr(href)"
+    # links_to_follow_XPATH = '/html/body//div/h2/a/@href'
+    click_button_selector_CSS = "ul.page-numbers li a.next" # CSS selector for the "Load more" button
+    click_button_selector_STOP_CSS = None # CSS for last "next page"-button (it still has a link but is disabled so it would keep loading the same articles on the last page indefinitely)
+                                                # If this is not relevant, delete this query and delete the argument stop_when_button_has_class in the 
+                                                # Dynamic_Scrapy_Click.fetch_links_with_clicking function (inside parse), or set it to it's default value - None
+    # FROM THE ARTICLE PAGE!!!
+    ''' CSS or XPath queries for relevant information found on the individual article pages.
+        These can obviously be omittet if all relevant information can be scraped from the front page, in which case parse_article should be disabled.
+    '''
+    article_title_XPATH = '//*[@id="blog-post-title-meta-container"]/div/h1/text()'
+    article_text_XPATH = '//div[@class="progression-studios-blog-single-excerpt"]//text()'
+    author_XPATH = '/html/body//div[2]/div/div[3]/span[1]/a/text()'
+    publication_date_CSS = 'meta[property="article:published_time"]::attr(content)'
+    image_links_XPATH = '//div[@class="progression-studios-blog-single-excerpt"]//img/@src'
+    external_links_XPATH = '//div[@class="progression-studios-blog-single-excerpt"]//@href'
+    article_categories = '//div[@class="tags-progression"]//text()'
+    embedded_media_links_XPATH = '//div[contains(@href, "youtube.com")]/@href'
+    # ... other queries
+
+    ### IMPORTANT FUNCTIONS FOR SETUP THAT CANNOT BE OMITTED AND PARAMETERS SHOULD NOT BE CHANGED! ###
+    def __init__(self, max_scrolls=None, max_pages=None):
+        """Initializes the spider and sets optional max_scrolls and max_pages limit."""
+        super().__init__()
+        Dynamic_Scrapy_Click.initialize(self, max_pages) # Initializes clicking setup
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs): 
+        """Creates the spider instance from crawler and sets it up."""
+        spider = super(DynamicSpider, cls).from_crawler(crawler, *args, **kwargs)
+        Dynamic_Scrapy_Click.setup_from_crawler(spider, crawler) # See doc string
+        return spider
+
+    def open_spider(self, spider):
+        """Executes setup actions when the spider is opened."""
+        self.logger.info("open_spider() is running!")
+        self.existing_data = Dynamic_Scrapy_Click.load_existing_links(self.save_path) # See doc string
+
+    ### THE ACTUAL SPIDER FUNCTIONALITY ###
+    @inlineCallbacks
+    def parse(self, response): # Can't be renamed
+        # Collects all article links dynamically using Playwright by clicking "Load more" button cumulatively
+        url = response.url
+        links = yield deferToThread(
+            asyncio.run,
+            Dynamic_Scrapy_Click.fetch_links_with_clicking(
+                url,
+                click_button_selector=self.click_button_selector_CSS,
+                links_selector=self.links_to_follow_CSS.replace('::attr(href)', ''), # Adjusts selector for link elements
+                max_clicks=self.max_pages,
+                wait_time=2000,
+                stop_when_button_has_class=self.click_button_selector_STOP_CSS
+            )
+        )
+        links = [urljoin(url, l) for l in links if l] # Ensures proper joining and non-None links
+
+        self.logger.info(f"Found {len(links)} article links on {url}")
+        for link in links:
+            self.logger.debug(f"Article link: {link}")
+
+        collected_items = []
+        
+        for link in links:
+            if link in self.existing_data: # Only scrapes information for articles that have not yet been scraped
+                self.logger.info(f"Skipping duplicate article: {link}")
+                continue
+
+            # Fetch each article manually
+            article_page = yield deferToThread(asyncio.run, Dynamic_Scrapy_Click.fetch_page_with_playwright(link))
+            article_sel = Selector(text=article_page)
+
+            # Parse and yield the article
+            item = self.parse_article(article_sel, link) # Goes to each article and scrapes relevant information
+            if item: # If any information has been scraped the data is added to 'collected_items'
+                collected_items.append(item)
+        
+        returnValue(collected_items) # MUST!!! be 'returnValue' since scrapy can't catch data from '@inlineCallbacks' using 'yield'!!!
+
+    def parse_article(self, response, article_link): # Can be renamed. IF IT IS REMEBER TO REDIRECT THE CALLBACK IN PARSE!
+        items = ScrapersItem() # Makes the items from items.py accessable within this function
+        # Extract 'scrape_date'
+        timestamp = datetime.now().strftime('%Y-%m-%d')
+        # Extract 'article_title'
+        article_title = response.xpath(self.article_title_XPATH).get() # .get() returns only the first element. Use .getall() to return a list of all elements if more than one element is expected
+        #article_title_clean = General_Functions.clean_text(article_title) # Cleans the text - See doc string
+        # Extract 'article_text'
+        article_text_bits = response.xpath(self.article_text_XPATH).getall() 
+        article_text_clean = General_Functions.join_and_clean(article_text_bits) # Joins and cleans all text elements - See doc string
+        author = response.xpath(self.author_XPATH).getall()
+        # Extract 'publication_date' 
+        publication_date = response.css(self.publication_date_CSS).get()
+        # Extract 'image_links' 
+        image_links = response.xpath(self.image_links_XPATH).getall()
+        # Extract 'article_HTML'
+        article_HTML = response.get()
+        article_categories = response.xpath(self.article_categories).getall()
+        article_categories_clean = [c.strip() for c in article_categories if c.strip()]
+        # Extract "embedded_media_links"
+        embedded_media_links = response.xpath(self.embedded_media_links_XPATH).getall()
+        # Extract "external_links"
+        external_links = response.xpath(self.external_links_XPATH).getall()
+
+        # Assign variables to items here
+        items['scrape_date'] = timestamp
+        items['publication_date'] = publication_date
+        items['source'] = self.source
+        items['article_link'] = article_link
+        items['article_title'] = article_title
+        items['article_text'] = article_text_clean
+        items['author'] = author
+        items['article_HTML'] = article_HTML # Retract and assign the full HTML (often article_title where not only text is retracted)
+        items['categories'] = article_categories_clean
+        items['embedded_media_links'] = embedded_media_links
+        items['image_links'] = image_links
+        items['external_links'] = external_links
+
+        self.logger.info(f"Scraped article: {article_title} ({article_link})") # Logs successful scrape
+
+        self.existing_data.add(article_link) # Adds article to list of scraped articles just in case multiple links from the front page directs to this article
+
+        return items # returns the items to parse
